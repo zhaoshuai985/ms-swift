f6a19f9f458c7fbe6318aac48f273fb531312f08
civi <civi@example.com>
2025-11-26 00:03:36 +0800

    Update prompts and scripts: add prompt4.txt with improved reasoning-answer alignment constraints

--- infer.sh
+++ infer.sh
@@ -48,11 +48,11 @@
 # sleep 30s
 # CUDA_VISIBLE_DEVICES=0 swift infer --val_dataset /data/datasets/vqarad/vqarad_test.jsonl --system examples/train/grpo/prompt.txt --max_new_tokens 512 --max_model_len 1024 --stream true --adapters /data/workspace/swift/output/v96-20251122-181946/checkpoint-6000
 # sleep 30s
-CUDA_VISIBLE_DEVICES=0 swift infer --val_dataset /data/datasets/vqarad/.archive/vqarad_test_251123.jsonl --system /data/workspace/swift/prompt2.txt --max_new_tokens 512 --stream true --adapters /data/workspace/swift/output/v97-20251124-020444/checkpoint-5500
-sleep 30s
-CUDA_VISIBLE_DEVICES=0 swift infer --val_dataset /data/datasets/vqarad/.archive/vqarad_test_251123.jsonl --system /data/workspace/swift/prompt2.txt --max_new_tokens 512 --stream true --adapters /data/workspace/swift/output/v97-20251124-020444/checkpoint-6000
-sleep 30s
-CUDA_VISIBLE_DEVICES=0 swift infer --val_dataset /data/datasets/vqarad/vqarad_test.jsonl --system /data/workspace/swift/prompt2.txt --max_new_tokens 512 --stream true --adapters /data/workspace/swift/output/v97-20251124-020444/checkpoint-5500
-sleep 30s
-CUDA_VISIBLE_DEVICES=0 swift infer --val_dataset /data/datasets/vqarad/vqarad_test.jsonl --system /data/workspace/swift/prompt2.txt --max_new_tokens 512 --stream true --adapters /data/workspace/swift/output/v97-20251124-020444/checkpoint-6000
+# CUDA_VISIBLE_DEVICES=0 swift infer --val_dataset /data/datasets/vqarad/.archive/vqarad_test_251123.jsonl --system /data/workspace/swift/prompt2.txt --max_new_tokens 512 --stream true --adapters /data/workspace/swift/output/v97-20251124-020444/checkpoint-5500
+# sleep 30s
+# CUDA_VISIBLE_DEVICES=0 swift infer --val_dataset /data/datasets/vqarad/.archive/vqarad_test_251123.jsonl --system /data/workspace/swift/prompt2.txt --max_new_tokens 512 --stream true --adapters /data/workspace/swift/output/v97-20251124-020444/checkpoint-6000
+# sleep 30s
+# CUDA_VISIBLE_DEVICES=0 swift infer --val_dataset /data/datasets/vqarad/vqarad_test.jsonl --system /data/workspace/swift/prompt3.txt --max_new_tokens 512 --stream true --adapters /data/workspace/swift/output/v99-20251124-233227/checkpoint-1500
+# sleep 30s
+CUDA_VISIBLE_DEVICES=0 swift infer --val_dataset /data/datasets/vqarad/vqarad_test.jsonl --system /data/workspace/swift/prompt3.txt --max_new_tokens 512 --stream true --adapters /data/workspace/swift/output/v106-20251125-131320/checkpoint-1500
 sleep 30s
